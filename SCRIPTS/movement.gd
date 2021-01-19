c59fd212a8c9f4ecdad5a29dd816044d1e62f47b
buttery-potato <71145077+buttery-potato@users.noreply.github.com>
2021-01-19 10:33:50 -0800

    commit changes

--- SCRIPTS/movement.gd
+++ SCRIPTS/movement.gd
@@ -3,5 +3,8 @@ var velocity = Vector3(0, 0, 0)
 func _physics_process(delta):
 	if Input.is_action_pressed("w"):
 		velocity.z = 30
-
+	elif Input.is_action_pressed("s"):
+		velocity.z = -30
+	velocity.z = lerp(velocity.z, 0, .2)
 	move_and_slide(velocity)
+	$Label.set_text(str(get_position_in_parent()))
